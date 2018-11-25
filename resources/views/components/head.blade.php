7df96d948726069d8416839607d069a4c31535cb
Hans Sulistio <hanslsulistio98@gmail.com>
2018-11-25 20:21:37 +0700

    Initial commit

---
+++ resources/views/components/head.blade.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="../../../css/style.css">
---
+++ resources/views/entry/login.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	@include('components.head')
+	<link rel="stylesheet" type="text/css" href="kok.css">
+</head>
+<body>
+	<div class="navbarr">
+		asdasds
+	</div>
+</body>
+</html>
